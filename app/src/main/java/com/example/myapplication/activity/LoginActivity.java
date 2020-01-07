254de73f2fc9a678e1de5628d658191231ee7d80
danilodionisia <danilo.dionisia@yahoo.com.br>
2020-01-07 10:51:02 -0300

    07-01-20 10:50

--- app/src/main/java/com/example/myapplication/activity/LoginActivity.java
+++ app/src/main/java/com/example/myapplication/activity/LoginActivity.java
@@ -1,16 +1,97 @@
 package com.example.myapplication.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.myapplication.R;
+import com.example.myapplication.config.ConfiguracaoFirebase;
+import com.example.myapplication.helper.UsuarioFirebase;
+import com.example.myapplication.model.Usuario;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthInvalidUserException;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText campoEmail, campoSenha;
+    FirebaseAuth autenticacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        campoEmail = findViewById(R.id.editTextLoginEmail);
+        campoSenha = findViewById(R.id.editTextLoginSenha);
+
+    }
+
+
+    public void validaLoginUsuario(View view){
+        String textoEmail = campoEmail.getText().toString();
+        String textoSenha = campoSenha.getText().toString();
+
+        if(!textoEmail.isEmpty()){
+
+            if(!textoSenha.isEmpty()){
+
+                Usuario usuario = new Usuario();
+                usuario.setEmail(textoEmail);
+                usuario.setSenha(textoSenha);
+
+                logarUsuario(usuario);
+
+            }else{
+                Toast.makeText(LoginActivity.this, "Preencha a senha!", Toast.LENGTH_SHORT).show();
+            }
+
+        }else{
+            Toast.makeText(LoginActivity.this, "Preencha o e-mail!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void logarUsuario(Usuario usuario) {
+
+        autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+        autenticacao.signInWithEmailAndPassword(
+                usuario.getEmail(), usuario.getSenha()
+        ).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if(task.isSuccessful()){
+
+                    UsuarioFirebase.redirecionaUsuarioLogado(LoginActivity.this);
+
+                }else {
+
+                    String excessao = "";
+
+                    try{
+                        throw task.getException();
+                    }catch (FirebaseAuthInvalidUserException e){
+                        excessao = "O usuário não está cadastrado!";
+                    }catch (FirebaseAuthInvalidCredentialsException e){
+                        excessao = "E-mail e Senha não correspondem a uma conta cadastrada";
+                    }catch (Exception e){
+                        excessao = "Erro ao cadastrar o usuário" + e.getStackTrace();
+                        e.printStackTrace();
+                    }
+
+                    Toast.makeText(LoginActivity.this, excessao, Toast.LENGTH_SHORT).show();
+
+                }
+
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/myapplication/activity/MainActivity.java
+++ app/src/main/java/com/example/myapplication/activity/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 
 import com.example.myapplication.R;
+import com.example.myapplication.helper.UsuarioFirebase;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,6 +28,13 @@ public class MainActivity extends AppCompatActivity {
 
     public void abrirTelaCadastro(View view){
         startActivity(new Intent(this, CadastroActivity.class));
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+
+        UsuarioFirebase.redirecionaUsuarioLogado(MainActivity.this);
     }
 
 }
